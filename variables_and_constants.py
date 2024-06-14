7ce4103a1436bd2e7e8b51caa61d2abd25dabf68
Syntax.Error <atikrah2010@gmail.com>
2024-06-14 06:08:41 +0600

    init comment

---
+++ variables_and_constants.py
@@ -0,0 +1,15 @@
+# Python Variables And Constants
+# declaring and assign value to constants
+
+PI = 3.14
+GRAVITY = 9.8
+
+print(f"PI : {PI}, GRAVITY : {GRAVITY}")
+
+# assigning multiple values in single line
+a,b,c,d = 1,2.5, 'Z', "atik"
+print(a,b,c,d)
+
+# assigning same value to multiple variables
+x = y = z = 1010
+print(x,y,z)
